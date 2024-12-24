f952fb68b98cd3e098c0720122790f1bb8b32ed3
Giorgio Bianchini <giorgio.bianchini@bristol.ac.uk>
2024-12-24 23:09:26 +0000

    Updated package versions.

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,5 +1,5 @@
 ﻿using MuPDFCore;
-using System.IO;
+using MuPDFCore.StructuredText;
 
 namespace Demo
 {
@@ -12,18 +12,18 @@ namespace Demo
 
             //Open a PDF document
             using MuPDFDocument doc1 = new MuPDFDocument(ctx, "Document1.pdf");
-            
+
             //Save the page as a PNG image with transparency, at a 1x zoom level (1pt = 1px).
             doc1.SaveImage(0, 1, PixelFormats.RGBA, "Raster1.png", RasterOutputFileTypes.PNG);
 
             //Open an Open XPS document
             using MuPDFDocument doc2 = new MuPDFDocument(ctx, "Document2.oxps");
-            
+
             //Save only part of the page as a PNG image with transparency, at a 2x zoom level (1pt = 2px).
             doc2.SaveImage(0, new Rectangle(87, 360, 517, 790), 2, PixelFormats.RGBA, "Raster2.png", RasterOutputFileTypes.PNG);
 
             //Merge the two documents into a single document.
-            MuPDFDocument.CreateDocument(ctx, "Merged.pdf", DocumentOutputFileTypes.PDF, true,
+            MuPDFDocument.Create.PDFDocument(ctx, "Merged.pdf",
 
                 //We take the full page from the first document
                 (doc1.Pages[0], doc1.Pages[0].Bounds, 1),
